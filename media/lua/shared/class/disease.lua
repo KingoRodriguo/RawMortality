7672988aa1e8ecd1d8a495168bdf6e0c6e5dce04
KingoRodriguo <rodrigue.martignat@live.fr>
2024-07-26 23:42:16 +0200

    refactor: Update Disease class parameters and remove unused code

--- media/lua/shared/class/disease.lua
+++ media/lua/shared/class/disease.lua
@@ -7,27 +7,19 @@ Disease = {}
 local diseasesList = {
     Allergie = {    --Disease parameter setup
         name = "Allergie",
-        isFatal = true, --Can player die from this disease
-        isPermanent = true, --Can this disease be cured
+        isPermanent = false, --Can this disease be cured
         isSymptomCurable = true, --Can drugs cure symptom of the disease
         isActive = false, --Does the symptoms should be applied
-        currentProgression = 0, --Current progression of the disease in the body
-        severity = 0.5, --Progression Multiplier (should be between 0 and 1)
 
-        maxAllergens = 3,
         Allergens = {}, --Set by applyPlayerAllergens()
     },
 
     Intolerance = {
         name = "Intolerance",
-        isFatal = false,
         isPermanent = true,
         isSymptomCurable = true,
         isActive = false,
-        currentProgression = 0,
-        severity = 0.2,
 
-        maxIntolerances = 3,
         Intolerances = {}, --Set by applyPlayerIntolerances()
     },
 } --]]
